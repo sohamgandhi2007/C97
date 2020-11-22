1eca835c27b47bb886dbf210c3ea7d5050d4e970
sohamgandhi2007 <63003386+sohamgandhi2007@users.noreply.github.com>
2020-11-22 22:04:43 +0530

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,13 @@
+intro=input("Guess a number (between 1 and 9)"): 
+ Enter your guess:-3
+ Your guess was too low: 
+ Guess something higher than 3
+  Enter your guess:-4
+ Your guess was too low: 
+ Guess something higher than 4
+  Enter your guess:-7
+ Your guess was too low: 
+ Guess something higher than 7
+ Enter your guess: - 9
+ Congratulation You Won!!!!
+ print(intro)
